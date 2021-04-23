50e1703d8905b4be8078c7160e7bf2023c71ba0b
luehkenecology <luehken65@gmail.com>
2021-04-23 07:30:15 +0200

    calculate percentage for each diagnostic class (doubtful, WNN negative, WNV positive)

--- 01-read_data.R
+++ 01-read_data.R
@@ -11,3 +11,12 @@ data_greece <- read.table("data/processed/wnv_results_eggs_greece.csv", header =
 library(magrittr)
 library(dplyr)
 
+# 
+data_greece2 <- data_greece %>%
+  group_by(Regional.Unit, Municipality, Village, classified.as) %>%
+  summarise(sum = length(classified.as))
+  
+# percentage 
+data_greece2 %>%
+  group_by(classified.as) %>%
+  summarise(x = sum(sum)/nrow(data_greece)*100)
