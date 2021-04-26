9b654b87849230dd60e6b2cba659920318e28f36
luehkenecology <luehken65@gmail.com>
2021-04-26 17:30:38 +0200

    produce first drafts for table 1 and fig 1

--- 01-read_data.R
+++ 01-read_data.R
@@ -3,13 +3,14 @@ RPROJ <- list(PROJHOME = normalizePath(getwd()))
 attach(RPROJ)
 rm(RPROJ)
 setwd(PROJHOME)
-
+ 
 # read table greece
 data_greece <- read.table("data/processed/wnv_results_eggs_greece.csv", header = T, sep = ";")
 
 # libraries
 library(magrittr)
 library(dplyr)
+library(tidyverse)
 
 # number of tested eggs
 nrow(data_greece)
@@ -33,30 +34,47 @@ total_eggs_region <- data_greece2 %>%
 # calculate the total number of eggs per regional unit and calender week
 total_eggs_region <- data_greece2 %>%
   group_by(Regional.Unit) %>%
-  summarise( =sum(sum))
+  summarise(total_n =sum(sum))
             
 #
 data_greece3 <- data_greece2 %>%
-  group_by(Regional.Unit, week, classified.as) %>%
+  group_by(Regional.Unit, week, classified.as, .drop = F) %>%
   summarise(n = sum(sum),
-            perc = sum(sum)/nrow(data_greece)*100)
-
-data_greece4 <- merge(data_greece3, total_eggs_region, by = "Regional.Unit")
-data_greece4$total_n
-library(ggplot2)
-
-data_greece4$classified.as <- factor(data_greece4$classified.as, levels = rev(c("WNV negative", "doubtful", "WNV positive")))
+            perc = sum(sum)/nrow(data_greece)*100) %>%
+  group_by(Regional.Unit, week) %>%
+  mutate(sum_region_week = sum(n)) %>%
+  group_by(Regional.Unit) %>%
+  mutate(sum_region = sum(n))
 
 
+
+
+head(data.frame(data_greece3))
+
+data_greece4 <- rbind(data.frame(data_greece3[,c(1:4,7)]),
+      data.frame(data_greece3[,1:2],
+                 classified.as = "tested",
+                 n = data_greece3$sum_region_week,
+                 sum_region = data_greece3$sum_region))
+
+# table per regionl unit
+ost <- data_greece4 %>%
+  group_by(Regional.Unit, classified.as) %>%
+  summarise(sum = sum(n))%>%
+  spread(classified.as, sum)
+write.table(cbind(ost[,1:4],
+                  tested = rowSums(ost[,2:4])), "ost.csv", sep = ";", col.names = NA)
+
+data_greece4$classified.as <- factor(data_greece4$classified.as, levels = rev(c("tested","WNV negative", "doubtful", "WNV positive")))
+
+# figure phenology
 data_greece4 %>%
-  filter(total_n > 100) %>%
-  ggplot(aes(week, n, fill = classified.as)) +
-  geom_bar(stat='identity') +
-  #geom_line(aes(color = classified.as))+
-  #geom_point(aes(color = classified.as))+
-  #geom_line() +
+  filter(sum_region > 100 & classified.as %in% c("tested", "doubtful", "WNV positive")) %>%
+  ggplot(aes(week, n, color = classified.as)) +
+  geom_point()+
+  geom_line() +
   ylab("number of eggs") +
   xlab("calendar week") +
   facet_wrap(~Regional.Unit) +
-  scale_fill_manual(values=rev(c("black", "yellow", "red")))+
+  scale_color_manual(values=rev(c("black", "yellow", "red")))+
   theme_bw()
